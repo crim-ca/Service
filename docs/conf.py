3f33069882582b98ca911bad9ffbeacadfaf15f9
Fred Osterrath <osterrfr@crim.ca>
2016-04-18 15:14:27 -0400

    Change user guide to reflect the Renaming of the package.

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ import sys, os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('../..'))
 
-from Service.__meta__ import __version__
+from VestaService.__meta__ import __version__
 
 # -- General configuration -----------------------------------------------------
 
