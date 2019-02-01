04ed3fac922d8377e1dabd0dd44498ea3cb87259
Frédéric Osterrath <osterrfr@crim.ca>
2019-02-01 12:49:04 -0500

    Move sentry onto it's own file

--- VestaService/__init__.py
+++ VestaService/__init__.py
@@ -2,10 +2,6 @@
 Meta values for current package.
 """
 
-import sentry_sdk
-
 __version__ = "0.4.0"
 __email__ = "osterrfr@crim.ca"
 __author__ = "Frederic Osterrath"
-
-sentry_sdk.init()
---
+++ VestaService/sentry_agent.py
@@ -0,0 +1,4 @@
+import sentry_sdk
+
+# Uses the SENTRY_DSN environment variable. See sentry documentation.
+sentry_sdk.init()
