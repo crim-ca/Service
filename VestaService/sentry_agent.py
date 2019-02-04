d1dd12e04fd93202196377dfa267d29a8374a634
Louis-David Perron <louis-david.perron@crim.ca>
2019-02-04 10:51:45 -0500

    Added Celery integration to sentry agent

--- VestaService/sentry_agent.py
+++ VestaService/sentry_agent.py
@@ -1,4 +1,5 @@
 import sentry_sdk
+from sentry_sdk.integrations.celery import CeleryIntegration
 
 # Uses the SENTRY_DSN environment variable. See sentry documentation.
-sentry_sdk.init()
+sentry_sdk.init(integrations=[CeleryIntegration()])
